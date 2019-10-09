ccab099ec0d177fddbfe19e6c878a10b2b5f9d84
MahmoudAbousetta6 <55044275+MahmoudAbousetta6@users.noreply.github.com>
2019-10-09 15:42:48 +0200

    hello world

---
+++ Assets/Abousetta/Scripts/TryAgainHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryAgainHandler : MonoBehaviour
+{
+    [SerializeField] Animator tryAgainAnimator;
+
+    void OpenBubble()
+    {
+        tryAgainAnimator.SetTrigger("IsOpened");
+    }
+    void CloseBubble()
+    {
+        tryAgainAnimator.SetTrigger("IsClosed");
+    }
+
+    public void ShowBubble()
+    {
+        OpenBubble();
+    }
+    public void HideBubble()
+    {
+        CloseBubble();
+    }
+
+
+}
