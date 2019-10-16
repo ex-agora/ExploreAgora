2ccef366581ef21a471cd3d5cc7dfb40ded3c93c
Amir Tarek <amir.hypatia@gmail.com>
2019-10-16 11:02:44 +0200

    Menu

--- Assets/Amir/Scripts/System/UI/MenuUIHandler.cs
+++ Assets/Amir/Scripts/System/UI/MenuUIHandler.cs
@@ -9,13 +9,16 @@ public class MenuUIHandler : MonoBehaviour
     [SerializeField] Sprite cloesMenuSprite;
     [SerializeField] Image menuBtnImg;
     [SerializeField] Animator menuAnimator;
+    [SerializeField] Animator menuBtnAnimator;
+    [SerializeField] Sprite[] audioSprites;
+    int indexSoundSprite;
     bool isOpen;
-
     public bool IsOpen { get => isOpen; set { isOpen = value; HandleMenu(); } }
 
     // Start is called before the first frame update
     void Start()
     {
+        indexSoundSprite = 0;
         IsOpen = false;
     }
    
@@ -25,14 +28,22 @@ public class MenuUIHandler : MonoBehaviour
 
     }
     void HandleMenusprite() {
-        if (IsOpen) { menuBtnImg.sprite = cloesMenuSprite; }
-        else { menuBtnImg.sprite = openMenuSprite; }
+        //if (IsOpen) { menuBtnImg.sprite = cloesMenuSprite; }
+        //else { menuBtnImg.sprite = openMenuSprite; }
         menuAnimator.SetBool("IsOpen", IsOpen);
+        menuBtnAnimator.SetBool("IsOpen", IsOpen);
     }
     void HandleMenu() {
         HandleMenusprite();
     }
     public void ClickToggleMenuPressed() {
         IsOpen = !IsOpen;
+    }
+    public void HandleSoundBtn(Image image) {
+        indexSoundSprite = (indexSoundSprite + 1) % audioSprites.Length;
+        image.sprite = audioSprites[indexSoundSprite];
+        if (AudioManager.Instance == null) {
+            AudioManager.Instance.AudioController(indexSoundSprite);
+        }
     }
 }
