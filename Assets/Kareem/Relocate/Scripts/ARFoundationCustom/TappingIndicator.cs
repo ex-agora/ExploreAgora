b8ec63c397e754d458e32848ccf7c7a63549e434
Kareem24-Tech <kareem.hypatia@gmail.com>
2019-10-16 10:42:22 +0200

    Plant parts - Hotspor

---
+++ Assets/Kareem/Relocate/Scripts/ARFoundationCustom/TappingIndicator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TappingIndicator : MonoBehaviour
+{
+
+    public interactions interactionsInstance;
+        [ContextMenu("Test")]
+    private void OnMouseDown()
+    {
+        if (interactionsInstance.canSet)
+            interactionsInstance.placeTheObject();
+    }
+}
---
+++ Assets/Kareem/Timer/scripts/TimerControllerAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using StateMachine;
+
+[CreateAssetMenu(fileName = "Timer Action", menuName = "SO /Variable/Timer Action", order = 0)]
+public class TimerControllerAction : Action
+{
+    // 
+    public override void Act<T>(StateControllersManager controllersManager)
+    {
+        TimerStateController timerStateController = controllersManager.GetController<TimerStateController>();
+        timerStateController.timerImage.fillAmount = 1 - timerStateController.StateMachineManager.ElpTime/ timerStateController.Duration;
+    }
+}
