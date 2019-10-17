1a9ffdb4ecdd94255182f3d331f54a2e86339130
mahmoudRabah <mahmoud.mostafarabah@gmail.com>
2019-10-17 15:47:53 -0700

    PlantPart - Model Interaction

--- Assets/Experiences/FlowerPlant/Abousetta/Scripts/Managers/PlantPartsGameManager.cs
+++ Assets/Experiences/FlowerPlant/Abousetta/Scripts/Managers/PlantPartsGameManager.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.ARFoundation;
 public class PlantPartsGameManager : MonoBehaviour
 {
     [SerializeField] StateMachineManager stateMachine;
+    static PlantPartsGameManager instance;
+    [SerializeField] Camera arCamera;
 
+    public static PlantPartsGameManager Instance { get => instance; set => instance = value; }
+    public Camera ArCamera { get => arCamera; set => arCamera = value; }
+
+    private void Awake ()
+    {
+        if ( Instance == null )
+            Instance = this;
+    }
     private void Start()
     {
         Invoke(nameof(StartMachin), 2f);
--- Assets/Rabah/Presentations/Plant Presentation/FaceCamera.cs
+++ Assets/Rabah/Presentations/Plant Presentation/FaceCamera.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class FaceCamera : MonoBehaviour
 {
-    public Transform Target;
+    Transform target;
+
+    public Transform Target { get => target; set => target = value; }
+    private void Start ()
+    {
+        Target =  PlantPartsGameManager.Instance.ArCamera.transform;
+    }
 
     void Update()
     {
-        Vector3 targetPostition = new Vector3(Target.position.x,
+        Vector3 targetPostition = new Vector3(-Target.position.x,
                                                 transform.position.y,
-                                                Target.position.z);
+                                                -Target.position.z);
         transform.LookAt(targetPostition);
     }
 }
