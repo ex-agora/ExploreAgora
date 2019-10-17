82f8a4eafff130028199c0d4103144f89d79550c
MahmoudAbousetta6 <55044275+MahmoudAbousetta6@users.noreply.github.com>
2019-10-17 15:50:20 +0200

    PlantPart-Tool UI Interaction

---
+++ Assets/Abousetta/Scripts/ToolBarHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBarHandler : MonoBehaviour
+{
+    [SerializeField] Animator uiSnapAnimator;
+
+    void ShowToolBar()
+    {
+        uiSnapAnimator.SetTrigger("IsOpened");
+    }
+    void HideToolBar()
+    {
+        uiSnapAnimator.SetTrigger("IsClosed");
+    }
+
+    public void OpenToolBar()
+    {
+        ShowToolBar();
+    }
+    public void CloseToolBar()
+    {
+        HideToolBar();
+    }
+}
