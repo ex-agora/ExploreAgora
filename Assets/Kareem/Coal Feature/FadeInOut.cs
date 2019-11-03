aef2a2fc654cb684b35af15ffc553c8ddf067399
Kareem24-Tech <kareem.hypatia@gmail.com>
2019-11-03 15:21:33 +0200

    OnBoarding-sicence-5%

---
+++ Assets/Kareem/Coal Feature/FadeInOut.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    private MeshRenderer gameObjecMat;
+    [SerializeField] GameEvent onFadeComplete;
+
+    private void Awake()
+    {
+        gameObjecMat = GetComponent<MeshRenderer>();
+    }
+
+    // false means fade out  
+    public void fadeInOut(bool state)
+    {
+        StartCoroutine(startFading(state, 0.5f));
+    }
+
+
+    IEnumerator startFading(bool state, float duration)
+    {
+
+        float elapsedTime = 0;
+        while (elapsedTime < duration)
+        {
+            if (state)
+                gameObjecMat.material.SetFloat("_Transparency", Mathf.Lerp(0, 0.5f, (elapsedTime / duration)));
+            else
+                gameObjecMat.material.SetFloat("_Transparency", Mathf.Lerp(0.5f, 0, (elapsedTime / duration)));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        //onComplete
+        if (onFadeComplete != null)
+            onFadeComplete.Raise();
+
+    }
+}
