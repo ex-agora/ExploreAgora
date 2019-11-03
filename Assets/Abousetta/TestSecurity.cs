2eacaab8045218629aa94e4c177be8a50174f3fe
MahmoudAbousetta6 <55044275+MahmoudAbousetta6@users.noreply.github.com>
2019-11-03 15:12:51 +0200

    Encryption

---
+++ Assets/Abousetta/TestSecurity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSecurity : MonoBehaviour
+{
+    string t;
+    public void TestEncryption(string input)
+    {
+        t = TestTEstEncrypt.Encrypt(input, "abcdefghijklmnop");
+        Debug.Log(t);
+    }
+    public void TestDecryption(string output)
+    {
+        if (output == string.Empty) output = t;
+        Debug.Log(TestTEstEncrypt.Decrypt(output, "abcdefghijklmnop"));
+    }
+}
