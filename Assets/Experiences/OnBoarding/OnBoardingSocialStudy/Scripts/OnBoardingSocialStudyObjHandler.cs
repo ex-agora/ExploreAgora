d7019f2b675c84115ab68761347236ac2765e06a
Amir Tarek <amir.hypatia@gmail.com>
2019-11-12 11:33:43 +0200

    SS - 80

---
+++ Assets/Experiences/OnBoarding/OnBoardingSocialStudy/Scripts/OnBoardingSocialStudyObjHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBoardingSocialStudyObjHandler : MonoBehaviour
+{
+    [SerializeField] Canvas hotspotCanvas;
+    public void ShowHotSpot() {
+        
+    }
+}
