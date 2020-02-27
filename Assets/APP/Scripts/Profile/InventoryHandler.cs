0d37c0e5f29effdec32fc06e26cbfc4443f9461c
MahmoudAbou6-Tech <52660876+MahmoudAbou6-Tech@users.noreply.github.com>
2020-02-27 23:32:52 +0200

    UI-UX Integration Update

---
+++ Assets/APP/Scripts/Profile/InventoryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryHandler : MonoBehaviour
+{
+    [SerializeField] private List<ScannedObjectHandler> scannedObjectHandlers;
+    [SerializeField] private List<string> scannedObjects;
+
+}
---
+++ Assets/APP/Scripts/Profile/ScannedObjectHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScannedObjectHandler : MonoBehaviour
+{
+    [SerializeField] private Image lockedImage;
+    [SerializeField] private Sprite unlockedImage;
+    [SerializeField] private Text tittle;
+    [SerializeField] private string name;
+
+    public string Name { get => name; }
+
+    private void UnlockObject()
+    {
+        lockedImage.sprite = unlockedImage;
+        tittle.gameObject.SetActive(true);
+    }
+}
