11f10aa9f02e64b5e653f8a22e2c78d001e35682
mahmoudRabah <mahmoud.hypatia@gmail.com>
2020-03-01 14:37:48 -0800

    Splash

---
+++ Assets/Abousetta/UI-UX/Scripts/TEST/SplashTest/SplashTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashTest : MonoBehaviour
+{
+    public Animator splashAnimator;
+    public Animator stonesAnimator;
+    public Animator fadeAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayAnim ();
+    }
+    void PlayAnim ()
+    {
+        splashAnimator.SetTrigger ("PlayLogoAnim");
+        stonesAnimator.SetTrigger ("PlayStonesAnim");
+        fadeAnimator.SetTrigger ("PlayFadeAnim");
+        Invoke (nameof (StopAnim) , 7f);
+    }
+    void StopAnim ()
+    {
+        splashAnimator.gameObject.SetActive (false);
+        stonesAnimator.gameObject.SetActive (false);
+    }
+}
