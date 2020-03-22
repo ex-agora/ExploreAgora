7036e820003be02d7702414d815f0ffc886171c0
Amir Tarek <amir.hypatia@gmail.com>
2020-03-22 11:41:34 +0200

    Country Handler - Update 89657

---
+++ Assets/APP/Scripts/Profile/CountryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountryHandler : MonoBehaviour
+{
+    [SerializeField] Dropdown countryDD;
+    int selectedCountryindex = -1;
+    private void Start()
+    {
+        countryDD.ClearOptions();
+        CountriesCodedInfo countriesInfo = CountryHolder.Instance.GetCountries();
+        int countrySize = countriesInfo.countries.Length;
+        List<string> countryNames = new List<string>();
+        for (int i = 0; i < countrySize; i++) {
+            countryNames.Add(countriesInfo.countries[i].country);
+        }
+        countryDD.AddOptions(countryNames);
+    }
+    public string GetCountryName() {
+        CountriesCodedInfo countriesInfo = CountryHolder.Instance.GetCountries();
+        return countriesInfo.countries[countryDD.value].country;
+    }
+}
