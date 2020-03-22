0a7dea57bad7aef51d9e64cd031f83f305b7af3c
Kareem24-Tech <kareem.hypatia@gmail.com>
2020-03-22 21:00:33 +0200

    onBoarding - flow 3
    9 PM 22/3

---
+++ Assets/Kareem/onBoarding Saving Progress/JoinUsBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class JoinUsBehavior : MonoBehaviour
+{
+    [SerializeField] ProfileInfoContainer profileInfoContainer;
+    [SerializeField] UnityEvent onSuccess , onFailed;
+
+   public void UpdateProfile()
+    {
+        ProfileData profileData = new ProfileData();
+        profileData.nickName = profileInfoContainer.nickname;
+        profileData.gender = profileInfoContainer.gender.ToString();
+        profileInfoContainer.DOB = profileInfoContainer.DOB;
+        profileInfoContainer.profileImgIndex = profileInfoContainer.profileImgIndex;
+
+        NetworkManager.Instance.UpdateProfile(profileData, OnUpdateProfileSuccess, OnUpdateProfileFailed);
+    }
+    private void OnUpdateProfileSuccess(NetworkParameters obj)
+    {
+        //flow
+        onSuccess.Invoke();
+    }
+    private void OnUpdateProfileFailed(NetworkParameters obj)
+    {
+        //ShowErrors (obj.err.message);
+        onFailed.Invoke();
+    }
+}
