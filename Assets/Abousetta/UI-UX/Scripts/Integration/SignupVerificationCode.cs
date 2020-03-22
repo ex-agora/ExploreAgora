dbb7fb26eef4cf0e90898f6becd925cfafbf9f0d
MahmoudAbou6-Tech <52660876+MahmoudAbou6-Tech@users.noreply.github.com>
2020-03-22 23:09:26 +0200

    Integration Update UI 2210

---
+++ Assets/Abousetta/UI-UX/Scripts/Integration/SignupVerificationCode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignupVerificationCode : MonoBehaviour
+{
+    [SerializeField] private SeperatedFields codeVerifcation;
+
+    public void VerifyMail()
+    {
+        VerifyEmailData v = new VerifyEmailData();
+        codeVerifcation.SubmitCode();
+        v.activationCode = codeVerifcation.code;
+        NetworkManager.Instance.VerifyEmail(v, OnVerifyMailSuccess, OnVerifyMailFailed);
+    }
+    private void OnVerifyMailSuccess(NetworkParameters obj)
+    {         //flow  
+
+    }
+    private void OnVerifyMailFailed(NetworkParameters obj)
+    { 
+        print(obj.err.message); 
+    }
+}
