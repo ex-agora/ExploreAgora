a07db41439d6c734b3a57caa89a27ba6272725d4
Kareem24-Tech <kareem.hypatia@gmail.com>
2020-04-11 14:21:07 +0200

    onBoarding New Edits
    11/4/2020

---
+++ Assets/Kareem/onBoarding Saving Progress/GettingIconHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class GettingIconHandler : MonoBehaviour
+{
+    [SerializeField] UnityEvent GettingBehavior;
+
+    public void DoGettingActions() {
+        GettingBehavior.Invoke();
+    }
+}
