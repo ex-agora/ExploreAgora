7424e581dbe4e0c85c9a2d1a5ef987b80e8efdde
MahmoudAbou6-Tech <52660876+MahmoudAbou6-Tech@users.noreply.github.com>
2020-06-17 05:18:32 +0200

    Statistical Charts - 180C

---
+++ Assets/Experiences/Statistical Charts/Scripts/StatisticalChartsManager.cs
@@ -0,0 +1,58 @@
+﻿using StateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StatisticalChartsManager : MonoBehaviour, ITriggable, IMenuHandler
+{
+    [SerializeField] StateMachineManager stateMachine;
+    [SerializeField] bool nextState;
+    [SerializeField] MenuUIHandler menu;
+
+    static StatisticalChartsManager instance;
+    int wrongTrialCount;
+
+    public static StatisticalChartsManager Instance { get => instance; set => instance = value; }
+    public int WrongTrialCount { get => wrongTrialCount; set => wrongTrialCount = value; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        WrongTrialCount = 0;
+    }
+    private void Start()
+    {
+        AudioManager.Instance.Play("bg", "Background");
+        Invoke(nameof(StartMachine), 2f);
+    }
+
+    public void StartFirstQuiz()
+    {
+        nextState = true;
+    }
+
+    public bool GetTrigger()
+    {
+        bool up = nextState;
+        nextState = false;
+        return up;
+    }
+
+    public void GoTOHome()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void StartMachine()
+    {
+        stateMachine.StartSM();
+    }
+
+}
---
+++ Assets/Presentations/Vuforia Presentations/StatisticalCharts/_StatisticalCharts/Scripts/GraphSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class GraphSlider : MonoBehaviour
+{
+    public Transform SliderOffsetParent;
+    //public TMP_Text SliderValueText;
+    public Text SliderValueText;
+    public Transform Target;
+    public GameObject Slider3DBar;
+    [HideInInspector]
+    public float SliderCheckResult;
+    private Slider slider;
+
+    public Slider Slider { get => slider; set => slider = value; }
+
+    private void Start()
+    {
+        Slider = GetComponent<Slider>();
+    }
+
+    public void SetSliderValue()
+    {
+        Slider3DBar.transform.localScale = new Vector3(Slider3DBar.transform.localScale.x, Slider.value/2, Slider3DBar.transform.localScale.z);
+        float BarTopPosition = Target.GetComponent<MeshRenderer>().bounds.extents.y;
+        SliderOffsetParent.position = new Vector3(SliderOffsetParent.position.x, Target.position.y + BarTopPosition, SliderOffsetParent.position.z);
+        SliderCheckResult = Slider.value * 5;
+        SliderValueText.text = (Slider.value*5).ToString();
+    }
+}
